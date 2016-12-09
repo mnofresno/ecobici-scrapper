9778bca606348c0a22bbcafbf01429d06cb600dd
Mariano <mnofresno@gmail.com>
2016-12-09 20:55:52 -0300

    Mejoro la logica de procesamiento de las llamadas a la api gcba

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var fs      = require("fs");
 var cheerio = require("cheerio");
 var exec = require('child_process').exec;
 
+var umbralBicicletas = 2;
 var estaciones = [ 17];
 /*
 var estaciones = [ ];
@@ -14,95 +15,108 @@ for(var i = 1; i < 83 ; i++)
     estaciones.push(i);
 }
 */
-var urlApiCiudad = function(estacionId) { return "http://epok.buenosaires.gob.ar/getObjectContent/?id=estaciones_de_bicicletas%7C" + estacionId; };
+var urlApiCiudad = function(estacionId) { return "http://epok.buenosaires.gob.ar/getObjectContent/?id=estaciones_de_bicicletas|" + estacionId; };
 
 //Lets define a port we want to listen to
 const PORT=7728; 
 
-
-fs.readFile('./estacion.html', function (err, estacionHtml)
-{
-    app(estacionHtml);
-});
-
-function app(estacionHtml)
-{
-    fs.readFile('./index.html', function (err, html)
+var traerDatosEstaciones = function(callback, callbackUmbral)
+{     
+    var outputEstaciones = [];
+    function receiveData(estacionIdBuscada)
     {
-        if (err) {
-            throw err; 
-        }    
-            
-        
-        //We need a function which handles requests and send response
-        function handleRequest(request, response)
+        var cargarInfo = function(data)
         {
-            var estacionesBuscadas = 0;
             
-            var $ = cheerio.load(html.toString());
+            var responseJs = JSON.parse(data);
             
-            response.writeHead(200, {'Content-Type': 'text/plain'});
+            var bicisDisponibles  = _.find(responseJs.contenido, { nombreId: 'bicicletas_disponibles'});
+            var nombreEstacion    = _.find(responseJs.contenido, { nombreId: 'nombre'});
+            var ubicacionEstacion = _.find(responseJs.contenido, { nombreId: 'ubicacion'});
+            outputEstaciones.push({ id: estacionIdBuscada, nombre: nombreEstacion.valor, ubicacion: ubicacionEstacion.valor, disponibles: bicisDisponibles.valor });
             
-            function receiveData(estacionIdBuscada)
+            if(!bicisDisponibles || !ubicacionEstacion || !nombreEstacion)
             {
-                var cargarInfo = function(data)
-                {
-                    response.writeHeader(200, {"Content-Type": "text/html"});  
-                    
-                    var responseJs = JSON.parse(data);
-                    
-                    var bicisDisponibles = _.find(responseJs.contenido, { nombreId: 'bicicletas_disponibles'});
-                    var nombreEstacion = _.find(responseJs.contenido, { nombreId: 'nombre'});
-                    var ubicacionEstacion = _.find(responseJs.contenido, { nombreId: 'ubicacion'});
-                    
-                    if(!bicisDisponibles || !ubicacionEstacion || !nombreEstacion)
-                    {
-                        $('body').append("<div>ESTACION NO DISPONIBLE: "+ estacionIdBuscada+"</div><span>DATA: </span>" + JSON.stringify(data));
-                        estacionesBuscadas++;
-                        return;
-                    }
-                    var estacionHtmlModificada = estacionHtml.toString();
-                    estacionHtmlModificada = estacionHtmlModificada.replace("{{ bicisDisponibles }}", bicisDisponibles.valor);
-                    estacionHtmlModificada = estacionHtmlModificada.replace("{{ ubicacionEstacion }}", ubicacionEstacion.valor);
-                    estacionHtmlModificada = estacionHtmlModificada.replace("{{ nombreEstacion }}", nombreEstacion.valor);
-                    if(parseInt(bicisDisponibles.valor)>2)
-                    {
-                        exec("beep", function(error, stdout, stderr) 
-                        {
-                            
-                        });
-                    }
-                    
-                    $('body').append(estacionHtmlModificada);
-                    estacionesBuscadas++;
-                    if(estacionesBuscadas === estaciones.length)
-                    {
-                        response.write($.html());
-                        response.end();
-                    }
-                };
-                return cargarInfo;
+                outputEstaciones.push({ id: estacionIdBuscada, nombre: "N/A", ubicacion: "N/A", disponibles: "N/A", data: JSON.stringify(data)  });
             }
             
-            _.forEach(estaciones, function(estacionId)
+            if(parseInt(bicisDisponibles.valor) > umbralBicicletas && callbackUmbral)
             {
-                najax({ url: urlApiCiudad(estacionId), success: receiveData(estacionId), contentType: "application/json" });
-            });
+                callbackUmbral();
+            }
             
-        }
-        
-        //Create a server
-        var server = http.createServer(handleRequest);
-        
-        //Lets start our server
-        server.listen(PORT, function(){
-            //Callback triggered when server is successfully listening. Hurray!
-            console.log("Server listening on: http://localhost:%s", PORT);
-        });
-    
-    
-    
+            if(outputEstaciones.length === estaciones.length && callback)
+            {
+                callback(outputEstaciones);
+            }
+        };
+        return cargarInfo;
+    }
+       
+    _.forEach(estaciones, function(estacionId)
+    {
+        najax({ url: urlApiCiudad(estacionId), success: receiveData(estacionId), error: receiveData(estacionId), contentType: "application/json" });
     });
+};
+
+function app()
+{
+    function handleRequest(request, response)
+    {
+        fs.readFile('./index.html', function (err, html)
+        {
+            if (err)
+            {
+                throw err; 
+            }    
+                
+            fs.readFile('./estacion.html', function (err, estacionHtml)
+            {
+                if (err)
+                {
+                    throw err; 
+                }    
+            
+                var $ = cheerio.load(html.toString());
+                
+                response.writeHeader(200, {"Content-Type": "text/html"});  
+                
+                var callback = function(outputEstaciones)
+                {
+                    _.forEach(outputEstaciones, function(estacion)
+                    {
+                        var estacionHtmlModificada = estacionHtml.toString();
+                        estacionHtmlModificada = estacionHtmlModificada.replace("{{ bicisDisponibles }}" , estacion.disponibles);
+                        estacionHtmlModificada = estacionHtmlModificada.replace("{{ ubicacionEstacion }}", estacion.ubicacion  );
+                        estacionHtmlModificada = estacionHtmlModificada.replace("{{ nombreEstacion }}"   , estacion.nombre     );
+                        
+                        $('body').append(estacionHtmlModificada);
+                    });
+                
+                    response.write($.html());
+                    response.end();
+                }; 
+                
+                traerDatosEstaciones(callback);
+
+            });
+        });       
+    }
+
+        
+    var server = http.createServer(handleRequest);
+    
+    server.listen(PORT, function()
+    {
+        console.log("Server listening on: http://localhost:%s", PORT);
+    });
+    
+    var intervalCallback = function(data)
+    {
+        exec("beep");
+    };
+    
+    setInterval( function(){ traerDatosEstaciones(null, intervalCallback); }, 1000);
 }
 
-
+app();
