5c4b73fbf16b5f2fa3362e4a00086c0974f8d6ee
Mariano <mnofresno@gmail.com>
2016-12-09 22:40:03 -0300

    Agrego carpeta public donde pongo los archivos que quiero exponer y permito también acceso a los node_modules para poderlos usar en deploy. Agrego handler diferenciado según las keywords de la request url.

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var _       = require("lodash");
 var http    = require("http");
 var fs      = require("fs");
 var cheerio = require("cheerio");
-var exec = require('child_process').exec;
-
+var exec    = require('child_process').exec;
+var path    = require('path');
 
 var alarmaActivada  = false;
 var umbralBicicletas = 2;
 var estaciones = [ 17 ];
+
+var baseFolder = "/public";
+var modulesFolder = "/node_modules";
 /*
 var estaciones = [ ];
 
@@ -62,71 +65,137 @@ var traerDatosEstaciones = function(callback, callbackUmbral)
         };
         return cargarInfo;
     }
-       
+    
     _.forEach(estaciones, function(estacionId)
     {
         najax({ url: urlApiCiudad(estacionId), success: receiveData(estacionId), error: receiveData(estacionId), contentType: "application/json" });
     });
 };
 
-function app()
+function handleSimpleFileRequest(request, response)
 {
-    
-    function handleRequest(request, response)
-    {
-        if(_.includes(request.url, "desactivar"))
-        {
-            alarmaActivada = false;
-            response.end("DESACTIVADA");
-            return;
-        }
-        else if(_.includes(request.url, "activar"))
-        {
-            alarmaActivada = true;
-            response.end("ACTIVADA");
-            return;
-        }
+    console.log('request starting...');
 
-        fs.readFile('./index.html', function (err, html)
+    var publicFile = "." + baseFolder + (request.url.charAt(0) === "/" ? request.url : "/" + request.url);
+    
+    var filePath = fs.existsSync(publicFile) ? publicFile :  '.' + modulesFolder + "/" + request.url;
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content)
+    {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile("." + baseFolder + "/404.html", function(error, content) {
+                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+}
+
+
+function handleSpecialRequest(request, response)
+{
+    if(_.includes(request.url, "desactivar"))
+    {
+        alarmaActivada = false;
+        response.end("DESACTIVADA");
+        return;
+    }
+    else if(_.includes(request.url, "activar"))
+    {
+        alarmaActivada = true;
+        response.end("ACTIVADA");
+        return;
+    }
+
+    fs.readFile("./" + baseFolder + "/index.html", function (err, html)
+    {
+        if (err)
+        {
+            throw err; 
+        }    
+            
+        fs.readFile("./" + baseFolder + "/estacion.html", function (err, estacionHtml)
         {
             if (err)
             {
                 throw err; 
             }    
-                
-            fs.readFile('./estacion.html', function (err, estacionHtml)
-            {
-                if (err)
-                {
-                    throw err; 
-                }    
+        
+            var $ = cheerio.load(html.toString());
             
-                var $ = cheerio.load(html.toString());
-                
-                response.writeHeader(200, {"Content-Type": "text/html"});  
-                
-                var callback = function(outputEstaciones)
+            response.writeHeader(200, {"Content-Type": "text/html"});  
+            
+            var callback = function(outputEstaciones)
+            {
+                _.forEach(outputEstaciones, function(estacion)
                 {
-                    _.forEach(outputEstaciones, function(estacion)
-                    {
-                        var estacionHtmlModificada = estacionHtml.toString();
-                        estacionHtmlModificada = estacionHtmlModificada.replace("{{ bicisDisponibles }}" , estacion.disponibles);
-                        estacionHtmlModificada = estacionHtmlModificada.replace("{{ ubicacionEstacion }}", estacion.ubicacion  );
-                        estacionHtmlModificada = estacionHtmlModificada.replace("{{ nombreEstacion }}"   , estacion.nombre     );
-                        
-                        $('body').append(estacionHtmlModificada);
-                    });
-                    $('#estadoAlarma').text((alarmaActivada ? "ACTIVADA" : "DESACTIVADA"));
-                    response.write($.html());
-                    response.end();
-                }; 
-                
-                traerDatosEstaciones(callback);
+                    var estacionHtmlModificada = estacionHtml.toString();
+                    estacionHtmlModificada = estacionHtmlModificada.replace("{{ bicisDisponibles }}" , estacion.disponibles);
+                    estacionHtmlModificada = estacionHtmlModificada.replace("{{ ubicacionEstacion }}", estacion.ubicacion  );
+                    estacionHtmlModificada = estacionHtmlModificada.replace("{{ nombreEstacion }}"   , estacion.nombre     );
+                    
+                    $('body').append(estacionHtmlModificada);
+                });
+                $('#estadoAlarma').text((alarmaActivada ? "ACTIVADA" : "DESACTIVADA"));
+                response.write($.html());
+                response.end();
+            }; 
+            
+            traerDatosEstaciones(callback);
 
-            });
-        });       
+        });
+    });       
+}
+
+var keywords = [ "index.html", "activar", "desactivar" ];    
+function handleRequest(request, response)
+{
+    if(request.url === "/" || _.includes(keywords, request.url.split("/").slice(-1).pop()))
+    {
+        return handleSpecialRequest(request, response);
     }
+    else
+    {
+        return handleSimpleFileRequest(request, response);
+    }   
+}    
 
+function app()
+{
         
     var server = http.createServer(handleRequest);
     
